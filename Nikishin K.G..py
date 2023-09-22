d24ab9f7b94e60f0598e638a3eb025fa2870b243
KirikNikishin <kirikNikishin45@gmail.com>
2023-09-23 01:56:53 +0300

    Добавлен модуль strings.py, удален текст

--- Nikishin K.G..py
+++ Nikishin K.G..py
@@ -1,10 +1,12 @@
 def plus(a, b):
     return a+b
 
-text = "new text"
-
 def minus(a, b):
     return a-b
+
+def krestik(a, b):
+    return a * b
 if __name__ == '__main__':
     print (plus(4, 5))
     print (minus(4, 2))
+    print (krestik(5,8))
